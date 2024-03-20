9922623838dd31f282e2af3fa76e19179950007d
Khouse169 <nguyenkhaihoandtd@gmail.com>
2024-03-20 21:27:49 +0700

    update html

---
+++ Client/Pages/Home/Detail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Client.Pages.Home
+{
+    public class DetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
